92c4756507f0f24340c62b069744bec00b2a8416
Herbapoux <yannick-poulin@hotmail.com>
2019-11-15 14:48:55 -0500

    Envoie de l'exercice 8

---
+++ Programmation1/Chapitre2/js/08.js
@@ -0,0 +1,36 @@
+/**
+ * Created by Yannick on 2019-15-04.
+ */
+
+//Faire un programme qui lit le taux horaire et le nombre d’heures travaillées d’un employé. Affichez son salaire. (Attention, il est payé le double de son taux horaire pour toutes les heures supplémentaires travaillées. Une semaine de travail normale est de 40 heures.
+
+
+    //Déclaration des variables
+var tauxHoraires;
+var nbHeures;
+var salaire;
+var tempsDouble;
+
+	//Lecture au clavier
+
+tauxHoraires = Number(prompt("Veillez entrer votre taux horaire"));
+nbHeures = Number(prompt("Veuillez entrer le nombre d'heures travaillées"));
+	
+	//Les calculs
+
+tempsDouble = (nbHeures - 40) * (2 * tauxHoraires);
+salaire = (tauxHoraires * nbHeures);
+
+	//Les affichages
+
+if(nbHeures <= 40){
+	alert("Votre salaire est de " + salaire + "$");
+}
+	else if(nbHeures > 40){
+		salaire = (tauxHoraires * 40) + tempsDouble;
+		alert("Votre salaire est de " + salaire + "$"); 
+	}
+
+else{
+	alert("Erreur sur la transcription de vos données")
+}
