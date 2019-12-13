b16666cc62ddd69ce94772d779814e1e8ddfbc17
Herbapoux <yannick-poulin@hotmail.com>
2019-12-13 14:13:27 -0500

    Envoie de l'exercice 6

--- Programmation1/Chapitre3&4/js/06.js
+++ Programmation1/Chapitre3&4/js/06.js
@@ -1,1 +1,14 @@
-// JavaScript Document
+/**
+ * Created by Yannick on 13-12-2019.
+ */
+
+/* 
+Question 06:
+Faire afficher 15 fois le mot "ATTENTION".
+*/
+
+
+for (var i=1; i<=15; i++){
+	document.write(i + " ATTENTION <br>");
+}
+
