59ef546768f46d1ce52f46bd17a76ac6f4cab6cd
Herbapoux <yannick-poulin@hotmail.com>
2019-12-13 11:10:14 -0500

    Envoie de l'exercice 4 (Chapitre 3&4)

---
+++ Programmation1/Chapitre3&4/js/04.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Yannick on 13-12-2019.
+ */
+
+/* 
+Question 04:
+Affichez 10 fois un décompte de 20 à 2.
+*/
+
+for(var nbFois=1; nbFois<=10; nbFois+=1 ){
+	for (var i=20; i>=2; i-=1){    
+		document.write(i +"<br>");  
+	}
+	document.write(nbFois + " fois <br>");
+}
+	
+
