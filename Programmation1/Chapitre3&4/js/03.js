751782bdc693afe4a6ee21c554047887ade655fe
Herbapoux <yannick-poulin@hotmail.com>
2019-12-13 11:01:01 -0500

    Envoie de l'exercice 3 (Chapitre 3&4)

---
+++ Programmation1/Chapitre3&4/js/03.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Yannick on 13-12-2019.
+ */
+
+/* 
+Question 03:
+Affichez tous les nombres pairs jusqu'à 10 000.
+*/
+
+
+for (var i=0; i<=10000; i+=2){    
+	document.write(i +"<br>");  
+}
+
