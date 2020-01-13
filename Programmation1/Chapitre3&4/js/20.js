819990ffeaa89da1cf3a23c409aa044098209166
Herbapoux <yannick-poulin@hotmail.com>
2020-01-13 10:02:03 -0500

    Envoie de l'exercice 20

---
+++ Programmation1/Chapitre3&4/js/20.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Yannick on 13-01-2020.
+ */
+
+/* 
+Question 20:
+Faites afficher 10 8 6 4 2 0 sur 12 lignes.
+*/
+
+for(var ligne = 1; ligne <= 12; ligne++){
+	for(var i = 10; i>=0; i-=2){
+		document.write(i + " ");
+	}
+	document.write ("<br>")
+}
