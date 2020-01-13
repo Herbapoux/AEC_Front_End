3e047e4aecfcecfd22c4d04e696e2c1fdf0a57d0
Herbapoux <yannick-poulin@hotmail.com>
2020-01-13 09:57:25 -0500

    Envoie de l'exercice 18 et 19

---
+++ Programmation1/Chapitre3&4/js/18.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Yannick on 13-01-2020.
+ */
+
+/* 
+Question 18:
+Faites afficher 1 2 3 4 5 6 7 8 9 10 sur 10 lignes.
+*/
+
+for(var ligne=1; ligne<=10; ligne++){
+	
+	for(var i=1; i<=10; i++){
+	document.write(i + " ");
+	}
+	document.write("<br>");
+}
+
---
+++ Programmation1/Chapitre3&4/js/22.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Yannick on 09-01-2020.
+ */
+
+/* 
+Question 22:
+*/
+
+for(var i=0; i<=5; i++){    
+	for(var j=0; j<=5; j++){        
+		document.write("[" + i + "," + j + "] " );    
+	}    
+	document.write("<br>");
+}
+
+
