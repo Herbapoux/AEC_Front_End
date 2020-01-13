16b0171fa8583919bb9437fc484a8518970bf55f
Herbapoux <yannick-poulin@hotmail.com>
2020-01-13 10:43:59 -0500

    Envoie de l'exercice 22

--- Programmation1/Chapitre3&4/js/22.js
+++ Programmation1/Chapitre3&4/js/22.js
@@ -1,16 +1,19 @@
 /**
- * Created by Yannick on 09-01-2020.
+ * Created by Yannick on 13-01-2020.
  */
 
 /* 
 Question 22:
+Faites fficher 	[0,0][0,1][0,2][0,3][0,4][0,5]
+				[1,0][1,1][1,2][1,3][1,4][1,5]
+				[2,0][2,1][2,2][2,3][2,4][2,5]
+				[3,0][3,1][3,2][3,3][3,4][3,5]
 */
 
-for(var i=0; i<=5; i++){    
-	for(var j=0; j<=5; j++){        
-		document.write("[" + i + "," + j + "] " );    
-	}    
+for(var ligne=0; ligne<=3; ligne++){
+	for(var i=0; i<=5; i++){
+		document.write("[" + ligne + "," + i + "]");
+	}
 	document.write("<br>");
 }
-
 
