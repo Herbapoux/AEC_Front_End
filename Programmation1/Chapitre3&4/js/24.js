c59fd6e3800521d2eb6f5c198d1c5450530d0916
Herbapoux <yannick-poulin@hotmail.com>
2020-01-14 15:57:26 -0500

    Envoie de l'exercice 24

--- Programmation1/Chapitre3&4/js/24.js
+++ Programmation1/Chapitre3&4/js/24.js
@@ -1,6 +1,6 @@
 /**
- * Created by Yannick on 13-01-2020.
- */
+* Created by Yannick on 14-01-2020.
+*/
 
 /* 
 Question 24:
@@ -11,3 +11,34 @@ Exemple :
 153 = = 13 + 53 + 33 = 1 + 125 + 27, est un nombre de Armstrong. 
 */
 
+var unite;
+var dizaine;
+var centaine;
+
+
+
+for(var i=1; i<1000; i++){
+	if (i<10){
+		unite = i;
+		if ((unite*unite*unite) === i){
+			document.write("Ceci est un nombre Armstrong " + i + "<br>");
+		}
+	}
+	else if (i>=10 && i<=99){
+		dizaine = Math.floor(i/10);
+		unite = i%10;
+		if ((unite*unite*unite)+(dizaine*dizaine*dizaine) === i){
+			document.write("Ceci est un nombre Armstrong " + i + "<br>");
+		}
+	}
+	else if (i>=100 && i<1000){
+		centaine = Math.floor(i/100);
+		dizaine = Math.floor((i-centaine*100)/10);
+		unite = i%10;
+		if( (unite*unite*unite)+(dizaine*dizaine*dizaine)+(centaine*centaine*centaine) === i){
+			document.write("Ceci est un nombre Armstrong " + i + "<br>");
+		}
+	}	
+}
+	
+	
