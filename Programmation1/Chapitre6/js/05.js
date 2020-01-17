a37d9a05faa4c89872deb334595d96a6b1098a7b
Herbapoux <yannick-poulin@hotmail.com>
2020-01-16 20:39:59 -0500

    Envoie de l'exercice 5

--- Programmation1/Chapitre6/js/05.js
+++ Programmation1/Chapitre6/js/05.js
@@ -8,14 +8,11 @@ Modifiez le numéro précédent pour trouver le plus grand nombre possible en Ja
 */
 
 var tab2Exp = [];
-var limite = 0;
 
-while(limite != 5){
-	for(var i=0; i<16; i++){
-		tab2Exp= Math.pow(2,i);
-		document.write("2 exposant " + i + " = " + tab2Exp + "<br>");
-		limite++
-	}	
-}
+for(var i=0; i<=1024; i++){
+	tab2Exp= Math.pow(2,i);
+	document.write("2 exposant " + i + " = " + tab2Exp + "<br>");
+}	
+
 
 	
