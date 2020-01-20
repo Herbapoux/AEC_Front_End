568cf1412223ebd2ecb27910f65de4fb43d7aae4
Herbapoux <yannick-poulin@hotmail.com>
2020-01-20 15:56:22 -0500

    Envoie de l'exercice 11

---
+++ Programmation1/Chapitre6/js/11.js
@@ -0,0 +1,39 @@
+/**
+ * Created by Yannick on 18-12-2019. Chapitre 6.
+ */
+
+/* 
+Question 11:
+C’est le temps du recensement. Nous aimerions connaître le nombre d’enfants que chaque famille possède et par la suite afficher un tableau de statistiques comme suit : Le nombre d’enfants est l’indice du tableau. 
+Regrouper ensemble les 10 enfants et plus.
+
+Truc : il faut utiliser l'indice du tableau pour représenter le nombre d'enfants.
+Par exemple : tab[2] = 10; signifie qu'il y a 10 familles qui ont 2 enfants.
+*/
+
+var nbEnfants = [0|0|0|0|0|0|0|0|0|0];
+var stats = false;
+
+
+for(var i=0; i<9999; i++){
+	nbEnfants[i] = Number(prompt("Combien d'enfant avez-vous? Écrire STOP pour afficher les stats."));
+	
+	if(nbEnfants === "STOP"){			
+		stats = true;
+	}
+}	
+
+
+if(stats){
+	document.write(i + "<br>");
+}
+
+else{
+	alert("erreur");
+}
+
+
+
+
+
+
