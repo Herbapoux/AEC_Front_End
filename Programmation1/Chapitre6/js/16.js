d0af81c4b29859151fbea1e5738dbb8945ea597f
Herbapoux <yannick-poulin@hotmail.com>
2020-01-30 15:08:00 -0500

    Envoie de l'exercice 16

--- Programmation1/Chapitre6/js/16.js
+++ Programmation1/Chapitre6/js/16.js
@@ -16,13 +16,25 @@ Générez un nombre aléatoire r.
 
 */
 
-var tableau =[1,2,3,4,5,6,7,8,9,10];
-var nbAleatoir =[];
+var tableau = [1,2,3,4,5,6,7,8,9,10];
+var nbAleatoir;
+var temp;
 
-for(var i=0; i<tableau.length-1; i++){
-	nbAleatoir[i] = Math.floor(Math.random()*10+1);
-	[tableau[i] = nbAleatoir[i]]
+document.write(tableau + "<br>");
+
+//mélanger le tableau
+for(var i=0; i<tableau.length; i++){
+	nbAleatoir = Math.floor(Math.random()*9);
+	console.log("On échange l'indice " + i + " avec l'indice " + nbAleatoir);
+	
+	temp = tableau[i];
+	tableau[i] = tableau[nbAleatoir];
+	tableau[nbAleatoir] = temp;
 }
 
-document.write(tableau + nbAleatoir);
+
+
+document.write(tableau);
+
+
 
