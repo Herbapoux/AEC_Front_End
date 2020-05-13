9b931a62dc9cdc77fa5bcb0e2f81d49bf223da0f
Herbapoux <yannick-poulin@hotmail.com>
2020-05-12 20:12:23 -0400

    mise a jour

--- Programmation2/thelastdance/src/App.js
+++ Programmation2/thelastdance/src/App.js
@@ -1,52 +1,109 @@
 import React from 'react';
-import logo from './img/netflixLogo.png';
 import steveKerrImg from './img/steveKerr.jpg';
 import michaelJordanImg from './img/michaelJordan.jpg';
 import billWenningtonImg from './img/billWennington.jpg';
 import scottiePippenImg from './img/scottiePippen.jpg';
 import dennisRodmanImg from './img/dennisRodman.jpg';
 import './App.css';
+import logo from './img/netflixLogo.png';
 
 const longueurLogo = "100 px";
 const longueurJoueurs = "250 px";
+
 let aleatoire = Math.floor(Math.random()*5);
-
-const nomJoueurs = ["Steve Kerr", "Micheal Jordan", "Bill Wennington", "Scottie Pippen", "Dennis Rodman"];
-const imagesJoueurs = [steveKerrImg, michaelJordanImg, billWenningtonImg, scottiePippenImg, dennisRodmanImg];
-
 const netflixLogo = (
   <img
   src ={logo}
   alt ="Netflix"
   height = {longueurLogo}
-  with = {longueurLogo}
   />
 )
+let imageJoueurs;
+const nomJoueurs = [
+  "Steve Kerr", 
+  "Micheal Jordan", 
+  "Bill Wennington", 
+  "Scottie Pippen", 
+  "Dennis Rodman"
+];
+
+const steve = 
+<img 
+  id="imgJoueur" 
+  src={steveKerrImg} 
+  height={longueurJoueurs} 
+  alt= "Steve Kerr"
+/>;
+
+const micheal = 
+<img 
+  id="imgJoueur" 
+  src={michaelJordanImg} 
+  height={longueurJoueurs} 
+  alt= "Micheal Jordan"
+/>;
+
+const bill = 
+<img 
+  id="imgJoueur" 
+  src={billWenningtonImg} 
+  height={longueurJoueurs} 
+  alt= "Bill Wennington"
+/>;
+
+const scottie = 
+  <img 
+  id="imgJoueur" 
+  src={scottiePippenImg} 
+  height={longueurJoueurs} 
+  alt= "Scottie Pippen"
+/>;
+
+const dennis = 
+  <img 
+  id="imgJoueur"
+  src={dennisRodmanImg} 
+  height={longueurJoueurs} 
+  alt= "Dennis Rodman"
+/>;
+
+// changement des noms avec l'image
+if (nomJoueurs[aleatoire] === "Steve Kerr"){
+  imageJoueurs = steve;
+}
+else if (nomJoueurs[aleatoire] === "Micheal Jordan"){
+  imageJoueurs = micheal;
+}
+else if (nomJoueurs[aleatoire] === "Bill Wennington"){
+  imageJoueurs = bill;
+}
+else if (nomJoueurs[aleatoire] === "Scottie Pippen"){
+  imageJoueurs = scottie;
+}
+else if (nomJoueurs[aleatoire] === "Dennis Rodman"){
+  imageJoueurs = dennis;
+}
 
 const nomJoueurAffiche =(
   nomJoueurs[aleatoire]
 )
 
-const imageJoueurs =(
-  <img
-    src = {imagesJoueurs[aleatoire]}
-    alt = {nomJoueurs[aleatoire]}
-    height = {longueurJoueurs}
-    with = {longueurJoueurs}
-  />
-) 
+//Création de la liste de joueur MAP à faire afficher
+const partants = nomJoueurs.map((element,i) => <li key={"element"+i}>{element}</li>);
 
-const partants = nomJoueurs.map(element => <li>{element}</li>);
+//Création de la liste de joueurs && à faire afficher
 const partants2 = (
-  <ul>
-    {aleatoire === 0 && <li key="li-00">Steve Kerr</li>}
-    {aleatoire === 1 && <li key="li-01">Micheal Jordan</li>}
-    {aleatoire === 2 && <li key="li-02">Bill Wennington</li>}
-    {aleatoire === 3 && <li key="li-03">Scottie Pippen</li>}
-    {aleatoire === 4 && <li key="li-04">Dennis Rodman</li>}
+  <ul id="partants2">
+    {imageJoueurs !== steve && <li>Steve</li>}
+    {imageJoueurs !== micheal && <li>Micheal</li>}
+    {imageJoueurs !== bill && <li>Bill</li>}
+    {imageJoueurs !== scottie && <li>Scottie</li>}
+    {imageJoueurs !== dennis && <li>Dennis</li>}
   </ul>
 );
 
+// Bouton évènement pour changer de joueur
+//const buttonChangerjoueur
 
 const texteAfficher = (
   <div>
