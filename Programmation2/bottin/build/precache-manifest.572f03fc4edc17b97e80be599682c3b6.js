b0219c499280673cca4ffcbde399d0d2cc181607
Herbapoux <yannick-poulin@hotmail.com>
2020-05-20 07:11:22 -0400

    mise en ligne

---
+++ Programmation2/bottin/build/precache-manifest.572f03fc4edc17b97e80be599682c3b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea73ee2b9296b5334b23ecac6e1a9cd5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6aa020e65f8bd79f9d5d",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8cd0cd514ffe4ed78880",
+    "url": "./static/js/2.97922e55.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.97922e55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aa020e65f8bd79f9d5d",
+    "url": "./static/js/main.9a474b27.chunk.js"
+  },
+  {
+    "revision": "1f21620924c021a81b15",
+    "url": "./static/js/runtime-main.590e4655.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
